16cd0f439f18bcd9796f956a8c316bdea1410b7e
oleweps <weps6179@gmail.com>
2023-03-25 04:02:08 -0700

    prints a triangle, followed by a new line

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,33 +1,35 @@
 #include "main.h"
 
 /**
-  *print_triangle - print a triangle aligned right using "#"
-  *@size: size of the triangle
+  * print_triangle - Prints a triangle of squares according parameter
+  * @size: The size of the squares triangle
+  *
+  * Return: empty
   */
-
 void print_triangle(int size)
 {
-	int c, i, j;
+	int x, y, z;
 
-	c = 0;
-	i = size - 1;
-	while (c < size)
-	{
-		i = size - 1 - c;
-		j = c + 1;
-		while (i > 0)
-		{
-			_putchar("#");
-			i--;
-		}
-		while (j > 0)
-		{
-			_putchar("#");
-			j--;
-		}
-		_putchar("\n");
-		c++;
-	}
 	if (size <= 0)
-		_putchar("\n");
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = size - x; y > 1; y--)
+			{
+				_putchar(32);
+			}
+
+			for (z = 0; z <= x; z++)
+			{
+				_putchar(35);
+			}
+
+			_putchar('\n');
+		}
+	}
 }
+
