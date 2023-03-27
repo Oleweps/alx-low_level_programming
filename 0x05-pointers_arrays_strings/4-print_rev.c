1879ebeac93cf5144a8bdb6f69cf0ea87b2f320e
oleweps <weps6179@gmail.com>
2023-03-27 07:42:48 -0700

    prints a string, in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,13 @@
 void print_rev(char *s)
 {
 	int a, b;
-
+	
 	b = 0;
-	while (*s[b] != '\0')
-		b++
-			for (a = b - 1; a >= 0; a--)
-			{
-				_putchar(*s[a]);
-			}
+	while (s[b] != '\0')
+		b++;
+	for (a = b - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
 	_putchar('\n');
 }
