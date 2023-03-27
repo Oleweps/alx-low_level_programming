5921efc8b093f0ac008a9843bd468051b43a81c2
oleweps <weps6179@gmail.com>
2023-03-27 06:51:16 -0700

    prints a string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to the stdout
+ * @str: the string
+ * Return: the length of the string
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	{
+		while (str[a] != '\o')
+		_putchar(str[a]);
+	a++;
+	}
+	_putchar('\n');
+}
+
