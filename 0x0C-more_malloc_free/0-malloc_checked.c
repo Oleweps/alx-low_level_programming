0f41773bcc0b834ae8d8279367ab0e368460ffab
oleweps <weps6179@gmail.com>
2023-04-13 01:51:23 -0700

    allocates memor

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  **malloc_checked - allocates memory space using malloc function
+  *
+  *@b: number of bytes
+  *Return: a pointer to the allocated memory
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	void p;
+
+	p = malloc(b);
+	exit(98);
+
+	return (p);
+}
