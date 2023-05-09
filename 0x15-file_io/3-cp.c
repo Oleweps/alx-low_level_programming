fb98135dc3b34e878c86be2bd5d8bb9107b8c05a
oleweps <weps6179@gmail.com>
2023-05-09 09:50:37 +0000

    Program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 char *create_buffer(char *file);
 void close_file(int fd);
-int main(int argc, char *argv[]);
 
 /**
   *create_buffer - allocates 1024 bytes for a buffer
